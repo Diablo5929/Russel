d9eaf50b89b15a4131af4231f995ce87f2ea418e
Diablo <hanon.mickael59@gmail.com>
2025-04-25 15:25:25 +0200

    first commit

---
+++ models/catway.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const catwaySchema = new mongoose.Schema({
+  catwayNumber: {
+    type: Number,
+    require: true,
+  },
+
+  type: {
+    type: String,
+    require: true,
+    lowercase: true,
+    trim: true,
+    enum: ["long", "short"],
+    message: "Must be long or short.",
+  },
+
+  catwayState: {
+    type: String,
+    trim: true,
+  },
+});
+
+const Catway = mongoose.model("Catway", catwaySchema);
+module.exports = Catway;
